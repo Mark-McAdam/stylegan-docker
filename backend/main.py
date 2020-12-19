583db1acd7d3a490605de5cd1ce63b05c8bc08a3
Mark <mark.a.mcadam@gmail.com>
2020-12-18 16:02:17 -0800

    final changes before heroku deployment

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,39 @@
 # backend/main.py
-
+import time
+import config
+import inference
+import asyncio
 import uuid
-
 import cv2
 import uvicorn
+import numpy as np
+
 from fastapi import File
 from fastapi import FastAPI
 from fastapi import UploadFile
-import numpy as np
-from PIL import Image
 
-import config
-import inference
+from PIL import Image
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 
 
 app = FastAPI()
+
+
+async def generate_remaining_models(models, image, name: str):
+    executor = ProcessPoolExecutor()
+    event_loop = asyncio.get_event_loop()
+    await event_loop.run_in_executor(
+        executor, partial(process_image, models, image, name)
+    )
+
+
+def process_image(models, image, name: str):
+    for model in models:
+        output, resized = inference.inference(models[model], image)
+        name = name.split(".")[0]
+        name = f"{name.split('_')[0]}_{models[model]}.jpg"
+        cv2.imwrite(name, output)
 
 
 @app.get("/")
@@ -23,13 +42,17 @@ def read_root():
 
 
 @app.post("/{style}")
-def get_image(style: str, file: UploadFile = File(...)):
+async def get_image(style: str, file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
     model = config.STYLES[style]
+    start = time.time()
     output, resized = inference.inference(model, image)
     name = f"/storage/{str(uuid.uuid4())}.jpg"
     cv2.imwrite(name, output)
-    return {"name": name}
+    models = config.STYLES.copy()
+    del models[style]
+    asyncio.create_task(generate_remaining_models(models, image, name))
+    return {"name": name, "time": time.time() - start}
 
 
 if __name__ == "__main__":
